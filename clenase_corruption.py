a3074bcf902b00ba874afb91d83e88ff20eb7532
Felipe Bailez <bailez@usp.br>
2020-06-06 01:09:32 -0300

    cleaning of old data completed

--- clenase_corruption.py
+++ clenase_corruption.py
@@ -1,9 +1,13 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun  6 00:47:59 2020
+import pandas as pd
 
-@author: bailez
-"""
+path = r'/home/bailez/data/Projetos/repos/eae0419-trab/'
 
-
+old_df = pd.read_excel(path + '95-11.xlsx').iloc[1:,18:]
+def clear_old_data(x):
+    return str(x[:4])
+old_df.columns = old_df.iloc[0,:].apply(clear_old_data)
+old_df.index = old_df.iloc[:,0]
+old_df = old_df.iloc[2:,3:].T
+old_df.index = pd.to_datetime(old_df.index, format="%Y")
+old_df = old_df.apply(pd.to_numeric, errors="coerce")*10
+old_df = old_df.sort_index()
